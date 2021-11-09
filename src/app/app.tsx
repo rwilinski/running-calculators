13485876b098ca85272e71898bf9580e065c5287
rwilinski <wileeek@gmail.com>
2021-11-09 16:49:44 +0100

    Initial color palette

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -7,6 +7,8 @@ import {
   Container,
   Paper,
 } from '@mui/material';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { grey } from '@mui/material/colors';
 
 import { PaceCalculator } from './modules/pace-calculator.module';
 import { TimeCalculator } from './modules/time-calculator.module';
@@ -16,13 +18,24 @@ import { PaceToSpeedConverter } from './modules/pace-to-speed-converter.module';
 
 import logo from './images/logo-35.png';
 
+const theme = createTheme({
+  palette: {
+    background: {
+      default: grey[200],
+    },
+    text: {
+      primary: grey[900],
+    },
+  },
+});
+
 export const App = () => {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <CssBaseline />
 
       <Box sx={{ flexGrow: 1 }}>
-        <AppBar position="static" sx={{ mb: 2 }}>
+        <AppBar position="static" sx={{ mb: 3 }}>
           <Toolbar>
             <Box
               sx={{
@@ -39,36 +52,36 @@ export const App = () => {
           </Toolbar>
         </AppBar>
 
-        <Container maxWidth="md" sx={{ mb: 3 }}>
+        <Container maxWidth="md" sx={{ mb: 4 }}>
           <Paper sx={{ p: 2 }}>
             <PaceCalculator />
           </Paper>
         </Container>
 
-        <Container maxWidth="md" sx={{ mb: 3 }}>
+        <Container maxWidth="md" sx={{ mb: 4 }}>
           <Paper sx={{ p: 2 }}>
             <TimeCalculator />
           </Paper>
         </Container>
 
-        <Container maxWidth="md" sx={{ mb: 3 }}>
+        <Container maxWidth="md" sx={{ mb: 4 }}>
           <Paper sx={{ p: 2 }}>
             <DistanceCalculator />
           </Paper>
         </Container>
 
-        <Container maxWidth="md" sx={{ mb: 3 }}>
+        <Container maxWidth="md" sx={{ mb: 4 }}>
           <Paper sx={{ p: 2 }}>
             <SpeedToPaceConverter />
           </Paper>
         </Container>
 
-        <Container maxWidth="md" sx={{ mb: 3 }}>
+        <Container maxWidth="md" sx={{ mb: 4 }}>
           <Paper sx={{ p: 2 }}>
             <PaceToSpeedConverter />
           </Paper>
         </Container>
       </Box>
-    </>
+    </ThemeProvider>
   );
 };
